5870bca167952b2ba7311b0eb3e5832783e4054f
Gianluca Recchia <gianluca.recchia97@gmail.com>
2020-01-02 05:20:32 +0100

    Switch to tabs for indentation
    The file was indented with spaces, for some reason.

--- profile.ps1
+++ profile.ps1
@@ -21,60 +21,60 @@ $PSDefaultParameterValues['*:Encoding'] = 'utf8'
 
 # Alias common parameters for Remove-Item
 function Nuke-Item {
-    Remove-Item -Recurse -Force @Args
+	Remove-Item -Recurse -Force @Args
 }
 
 # Create an alias to make it easier to open text files
 function Edit-File {
-    param(
-        [Parameter(Mandatory=$True, Position=0)]
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $File,
+	param(
+		[Parameter(Mandatory=$True, Position=0)]
+		[ValidateNotNullOrEmpty()]
+		[String]
+		$File,
 
-        [ValidateNotNullOrEmpty()]
-        [String]
-        $Editor=$env:VISUAL,
+		[ValidateNotNullOrEmpty()]
+		[String]
+		$Editor=$env:VISUAL,
 
-        [String[]]
-        $EditorArgs
-    )
+		[String[]]
+		$EditorArgs
+	)
 
-    Invoke-Expression "$Editor $EditorArgs $File"
+	Invoke-Expression "$Editor $EditorArgs $File"
 }
 
 # Create an alias to make it easier to open the Powershell configuration
 function PS-Config {
-    param(
-        [String[]]
-        $EditorArgs
-    )
+	param(
+		[String[]]
+		$EditorArgs
+	)
 
-    Edit-File "$($Profile.CurrentUserAllHosts)" -EditorArgs "$EditorArgs"
+	Edit-File "$($Profile.CurrentUserAllHosts)" -EditorArgs "$EditorArgs"
 }
 
 # Stop the Emacs server
 function Stop-Emacs-Server {
-    while (Get-Process emacs -ErrorAction SilentlyContinue) {
-        emacsclientw.exe --eval '(kill-emacs)'
-        Start-Sleep -Milliseconds 1024
-    }
+	while (Get-Process emacs -ErrorAction SilentlyContinue) {
+		emacsclientw.exe --eval '(kill-emacs)'
+		Start-Sleep -Milliseconds 1024
+	}
 }
 
 # Start the Emacs server
 function Start-Emacs-Server {
-    if (Get-Process emacs -ErrorAction SilentlyContinue) {
-        echo 'Emacs is already running.'
-        return
-    }
-    Remove-Item -Recurse -Force "$HOME\.emacs.d\server\*"
-    runemacs.exe --daemon --chdir "$HOME"
+	if (Get-Process emacs -ErrorAction SilentlyContinue) {
+		echo 'Emacs is already running.'
+		return
+	}
+	Remove-Item -Recurse -Force "$HOME\.emacs.d\server\*"
+	runemacs.exe --daemon --chdir "$HOME"
 }
 
 # Restart the Emacs server
 function Restart-Emacs-Server {
-    Stop-Emacs-Server
-    Start-Emacs-Server
+	Stop-Emacs-Server
+	Start-Emacs-Server
 }
 
 # Check if ripgrep is installed before sourcing its completion database
