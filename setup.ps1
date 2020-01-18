0f077543f48e0ed740941f0a75c66707c3756fa5
Gianluca Recchia <gianluca.recchia97@gmail.com>
2020-01-18 12:42:57 +0100

    Delete the setup script
    It was too hard to maintain.

--- setup.ps1
+++
@@ -1,6 +0,0 @@
-# This script will perform the required setup to install the modules
-# and it's meant to be run from an elevated prompt
-Install-Module PowerShellGet -Scope CurrentUser -Force -AllowClobber
-
-PowerShellGet\Install-Module posh-git -AllowPrerelease
-PowerShellGet\Install-Module z
