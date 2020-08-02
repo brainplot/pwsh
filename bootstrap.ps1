dee97852fde48f2e11e57322a593ad98a551728f
Gianluca Recchia <gianluca.recchia97@gmail.com>
2020-08-03 00:41:25 +0200

    Remove bootstrap script
    It was basically ignored and never kept up-to-date, making it useless.

--- bootstrap.ps1
+++
@@ -1,7 +0,0 @@
-# Helper script to install all my PowerShell modules
-
-Install-Module PowerShellGet -Scope CurrentUser -Force -AllowClobber
-
-PowerShellGet\Install-Module posh-git -Scope CurrentUser -AllowPrerelease -Force
-PowerShellGet\Install-Module ZLocation -Scope CurrentUser -AllowPrerelease -Force
-PowerShellGet\Install-Module cd-extras -Scope CurrentUser -AllowPrerelease -Force
